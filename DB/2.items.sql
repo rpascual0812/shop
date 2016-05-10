51343f589f5b4e8102cb451e4e52050430811164
Rafael Pascual <rafael.pascual@team.acquireap.com>
2016-05-10 12:40:23 +0800

    Fixed insert items

--- DB/2.items.sql
+++ DB/2.items.sql
@@ -4,7 +4,8 @@ insert into items
 	item,
 	price,
 	description,
-	delivery_time
+	delivery_time,
+	image
 )
 values
 (
@@ -12,42 +13,48 @@ values
 	'Bazooka',
 	5,
 	'Your favorite bubble gum.',
-	'00:30:00'
+	'00:30:00',
+	'./ASSETS/uploads/201605/320x150.png'
 ),
 (
 	1,
 	'Pom Poms',
 	2,
 	'You missed this.',
-	'00:30:00'
+	'00:30:00',
+	'./ASSETS/uploads/201605/320x150.png'
 ),
 (
 	2,
 	'Pandesal',
 	5,
 	'Hot and fresh',
-	'00:30:00'
+	'00:30:00',
+	'./ASSETS/uploads/201605/320x150.png'
 ),
 (
 	3,
 	'Adobo',
 	100,
 	'Pork and Chicken - You choose.',
-	'00:30:00'
+	'00:30:00',
+	'./ASSETS/uploads/201605/320x150.png'
 ),
 (
 	4,
 	'Halo-Halo',
 	50,
 	'So Tasty.',
-	'00:30:00'
+	'00:30:00',
+	'./ASSETS/uploads/201605/320x150.png'
 ),
 (
 	5,
 	'Turkey',
 	200,
 	'Just for you.',
-	'00:30:00'
+	'00:30:00',
+	'./ASSETS/uploads/201605/320x150.png'
 );
 
 
